3a4598f2030455eb1a0e888b4582912f0136af59
Maximelio-Finch <87384802+max-proj17@users.noreply.github.com>
2025-06-02 00:03:14 -0500

    Starting attempt to latch CUDA to my docker instance

--- Self-Balancing-Bot-Pid/Simulation/run_balance_bot_container.sh
+++ Self-Balancing-Bot-Pid/Simulation/run_balance_bot_container.sh
@@ -6,16 +6,18 @@ WORKSPACE_DIR=$(pwd)/ros2_ws
 
 # setting DISPLAY manually (for X11 forwarding)
 export DISPLAY=$(ip route | awk '/default/ {print $3}'):0
+xhost +local:docker
 
 # Kill old container if running
 docker rm -f $CONTAINER_NAME 2>/dev/null
 
 # Run container
 docker run -it \
+    --gpus all \
     --name $CONTAINER_NAME \
     --rm \
     -e DISPLAY=$DISPLAY \
-	-e LIBGL_ALWAYS_INDIRECT=1 \
+    -e LIBGL_ALWAYS_INDIRECT=0 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $WORKSPACE_DIR:/home/dev/ros2_ws \
     $IMAGE_NAME
